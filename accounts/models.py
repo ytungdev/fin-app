da3ec3ce41c4077c593abbda882a73d1a2b350e3
ytung <ytung.dev@gmail.com>
2023-03-31 00:40:24 +0100

    added: 1data entry and data disply

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Accounts(models.Model):
+class Account(models.Model):
   location  = models.CharField(max_length=255)
   name      = models.CharField(max_length=255)
   provider  = models.CharField(max_length=255)
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Account
 
 
 def accounts(request):
-  template = loader.get_template('first_page.html')
-  return HttpResponse(template.render())
+  all_account = Account.objects.all().values()
+  template = loader.get_template('all_account.html')
+  context = {
+    'all_account': all_account,
+  }
+  return HttpResponse(template.render(context, request))
