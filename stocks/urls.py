47cbe7d6c56554cda85d5d4013f84b9e9bf49c18
ytung <ytung@camellia-Air>
2023-04-13 17:16:06 +0100

    added: stock details

--- stocks/urls.py
+++ stocks/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 app_name = 'stocks'
 urlpatterns = [
+    path('stocks/', views.StockList.as_view(), name='stocks'),    
     path('inouts/', views.inouts, name='inouts'),    
+    path('view_stock/', views.view_stock, name='view_stock'),    
+    path('view_stock/<int:stock_id>', views.view_stock, name='view_stock'),    
     path('add_inout/', views.add_inout, name='add_inout'),    
     path('add_inout/<int:stock_id>/', views.add_inout, name='add_inout'),    
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Sum
+from django.views import generic
+from django.contrib import messages
 
 from .models import Stock, StockInOut, StockRecord
 from .forms import StockForm
+
 from datetime import date
 import os
 import csv
-
 
 def format_symbol(sym):
     if sym.isdigit():
@@ -14,6 +16,53 @@ def format_symbol(sym):
     else:
         res = sym
     return res
+
+class StockList(generic.ListView):
+  template_name = 'stock_list.html'
+  context_object_name = 'all_stock'
+  def get_queryset(self):
+    return Stock.objects.all()
+
+def view_stock(request, stock_id=None):
+    ## recieve form
+    if request.method == 'POST':
+        form = StockForm(request.POST)
+        if form.is_valid():
+            symbol = format_symbol(form.cleaned_data['symbol'])
+            market= form.cleaned_data['market'].upper()
+            ## create new stock
+            if stock_id==None:
+                stock = Stock.objects.filter(symbol=symbol, market=market).values()
+                ## reject : stock exists
+                if stock:
+                    messages.error(request,f'stock exist : {stock[0]["symbol"]}')
+                    return redirect('/stock/view_stock')
+                else:
+                    stock = Stock()
+            ## update existing stock
+            else:
+                stock = get_object_or_404(Stock, pk=stock_id)
+            stock.market = market
+            stock.symbol = symbol
+            stock.name = form.cleaned_data['name']
+            stock.curr = form.cleaned_data['curr'].upper()
+            stock.save()
+            return redirect('/stock/stocks')
+    ## render form
+    else:
+        form = StockForm()
+        ## blank form
+        if stock_id==None:
+            stock = None
+        ## fetched form
+        else:
+            stock = get_object_or_404(Stock, pk=stock_id)
+            form.fields["market"].initial = stock.market
+            form.fields["symbol"].initial = stock.symbol
+            form.fields["name"].initial = stock.name
+            form.fields["curr"].initial = stock.curr
+        context = {'form':form, 'data':stock}
+        return render(request, 'stock_details.html', context)
 
 def inouts(request):
     inout = StockInOut.objects.order_by('-date').values()
@@ -30,52 +79,55 @@ def inouts(request):
     return render(request, 'inouts.html', context)
 
 def add_inout(request, stock_id=None):
-  if request.method == 'POST':
-    data = request.POST
-    d =  data.get("date") if data.get("date") else date.today()
-    #### debug
-    print(data, stock_id)
-    if stock_id==None:
-        sym = format_symbol(data.get("symbol"))
-        stock = Stock.objects.filter(symbol=sym).values()
-        if not stock:        
-            new = Stock()
-            new.name = data.get("name").strip()
-            new.symbol = sym
-            new.market = data.get("market").upper()
-            new.curr = data.get("curr").upper()
-            new.save()
-            stock_id = new.id
+    ## recieve form
+    if request.method == 'POST':
+        data = request.POST
+        d =  data.get("date") if data.get("date") else date.today()
+
+        ## create new stock
+        if stock_id==None:
+            sym = format_symbol(data.get("symbol"))
+            stock = Stock.objects.filter(symbol=sym).values()
+            if not stock:        
+                new = Stock()
+                new.name = data.get("name").strip()
+                new.symbol = sym
+                new.market = data.get("market").upper()
+                new.curr = data.get("curr").upper()
+                new.save()
+                stock_id = new.id
+            else:
+                messages.error(request,f'stock exist : {stock[0]["symbol"]}')
+                stock_id = stock[0]["id"]
+                return redirect(f"stock/add_inout/{stock_id}")
+        
+        ## create new reocrd
+        rec = StockInOut.objects.filter(stock_id=stock_id, date=d).values()
+        if rec:
+            messages.error(request,f'record exist : {d} - {stock_id} - {rec[0]["id"]} ')
         else:
-            stock_id = stock[0]["id"]
-    
-    rec = StockInOut.objects.filter(stock_id=stock_id, date=d).values()
-    if rec:
-       print(f"rec exist : {rec[0]['id']}")
+            new = StockInOut()
+            new.stock_id = stock_id
+            new.date = d
+            new.price = data.get("price")
+            new.unit = data.get("unit")
+            new.save()
+            print(f"saved : {new.id}")
+        return redirect(f'/stock/add_inout/{stock_id}')
+    ## render form
     else:
-        new = StockInOut()
-        new.stock_id = stock_id
-        new.date = d
-        new.price = data.get("price")
-        new.unit = data.get("unit")
-        new.save()
-        print(f"saved : {new.id}")
-    return redirect(f'/stock/add_inout/{stock_id}')
-  else:
-    form = StockForm()
-    if stock_id==None:
-        context = { 'data' : None, 'form' : form}
-        return render(request, 'add_stock_inout.html', context)
-    else:
-        stock = Stock.objects.filter(id=stock_id).values()
-        if not stock:
+        form = StockForm()
+        ## blannk form
+        if stock_id==None:
             context = { 'data' : None, 'form' : form}
             return render(request, 'add_stock_inout.html', context)
+        ## fetched form
         else:
-            form.fields["market"].initial = stock[0]["market"]
-            form.fields["symbol"].initial = stock[0]["symbol"]
-            form.fields["name"].initial = stock[0]["name"]
-            form.fields["curr"].initial = stock[0]["curr"]
+            stock = get_object_or_404(Stock, pk=stock_id)
+            form.fields["market"].initial = stock.market
+            form.fields["symbol"].initial = stock.symbol
+            form.fields["name"].initial = stock.name
+            form.fields["curr"].initial = stock.curr
 
             form.fields["market"].disabled = True
             form.fields["symbol"].disabled = True
@@ -84,8 +136,7 @@ def add_inout(request, stock_id=None):
 
             inout = StockInOut.objects.filter(stock_id=stock_id).order_by('-date').values()
             worths = []
-            gain = 0
-            bal = 0
+            gain, bal = 0, 0
             for rec in inout:
                 worth = rec["unit"] * rec["price"] * -1
                 gain += worth
